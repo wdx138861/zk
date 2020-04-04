b034885121c719747dd86afca956b8482e3f04fd
wdx <wdx8486@163.com>
2020-04-04 23:57:20 +0800

    zk测试例子

---
+++ zookeeper/src/main/java/com/wdx/zkclient/FluentTest.java
@@ -0,0 +1,56 @@
+package com.wdx.zkclient;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.CuratorWatcher;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.data.Stat;
+
+public class FluentTest {
+
+    //指定zk集群
+    private static final String CLUSTER = "127.0.0.1:2181";
+
+    //指定节点名称
+    private static final String PATH = "/mylog";
+
+    public static void main(String[] args) throws Exception  {
+        // ---------------- 创建会话 -----------
+        // 创建重试策略对象：第 1 秒重试 1 次，最多重试 3 次
+        ExponentialBackoffRetry retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        //创建客户端   链式编程
+        CuratorFramework client = CuratorFrameworkFactory.builder().connectString(CLUSTER).sessionTimeoutMs(15000)
+                .connectionTimeoutMs(13000).retryPolicy(retryPolicy).namespace("logs").build();
+
+        //开启客户端
+        client.start();
+
+        // 指定要创建和操作的节点，注意，其是相对于/logs 节点的
+        String nodePath = "/host";
+
+        // ---------------- 创建节点 -----------
+        String nodeName = client.create().forPath(nodePath, "myhost".getBytes());
+        System.out.println("新创建的节点名称为：" + nodeName);
+
+        // ---------------- 获取数据内容并注册 watcher -----------
+        byte[] data = client.getData().usingWatcher((CuratorWatcher) event -> {
+            System.out.println(event.getPath() + "数据内容发生变化");
+        }).forPath(nodePath);
+        System.out.println("节点的数据内容为：" + new String(data));
+
+        // ---------------- 更新数据内容 -----------
+        client.setData().forPath(nodePath, "newHost".getBytes());
+        // 获取更新过的数据内容
+        byte[] newData = client.getData().forPath(nodePath);
+        System.out.println("更新过的数据内容为：" + new String(newData));
+
+        client.delete().forPath(nodePath);
+
+        Stat stat = client.checkExists().forPath(nodePath);
+        boolean flag = true;
+        if (stat == null) {
+            flag = false;
+        }
+        System.out.println(nodePath + "节点仍存在吗？" + flag);
+    }
+}
--- zookeeper/src/main/java/com/wdx/zkclient/ZkClientTest.java
+++ zookeeper/src/main/java/com/wdx/zkclient/ZkClientTest.java
@@ -1,7 +1,66 @@
 package com.wdx.zkclient;
 
+import org.I0Itec.zkclient.IZkDataListener;
+import org.I0Itec.zkclient.ZkClient;
+import org.I0Itec.zkclient.serialize.SerializableSerializer;
+import org.apache.zookeeper.CreateMode;
+
 public class ZkClientTest {
 
     //指定zk集群
-    private static
+    private static final String CLUSTER = "127.0.0.1:2181";
+
+    //指定节点名称
+    private static final String PATH = "/mylog";
+
+    public static void main(String[] args) {
+        // 创建 zkClient
+        ZkClient zkClient = new ZkClient(CLUSTER);
+        // 为 zkClient 指定序列化器
+        zkClient.setZkSerializer(new SerializableSerializer());
+
+        // ---------------- 创建节点 -----------
+        // 指定创建持久节点
+        CreateMode mode = CreateMode.PERSISTENT;
+        // 指定节点数据内容
+        String data = "first log";
+        // 创建节点
+        String nodeName = zkClient.create(PATH, data, mode);
+        System.out.println("新节点名称为： " + nodeName);
+
+        // ---------------- 获取数据内容 -----------
+        Object readData = zkClient.readData(PATH);
+        System.out.println("节点数据为： " + readData);
+
+        // ---------------- 注册 watcher -----------
+        zkClient.subscribeDataChanges(PATH, new IZkDataListener() {
+            public void handleDataChange(String dataPath, Object data) throws Exception {
+                System.out.println("节点:" + dataPath + "的数据更新为： " + data);
+            }
+
+            public void handleDataDeleted(String dataPath) throws Exception {
+                System.out.println(dataPath + "的数据内容被删除");
+            }
+        });
+
+        zkClient.subscribeChildChanges(PATH, (parentPath, currentChilds) -> {
+
+        });
+
+        // ---------------- 更新数据内容 -----------
+        zkClient.writeData(PATH, "second log");
+        Object updatedData = zkClient.readData(PATH);
+        System.out.println("更新后的数据为：" + updatedData);
+
+        // ---------------- 更新数据内容 -----------
+        zkClient.writeData(PATH, "third log");
+        System.out.println("更新后的数据为：" + zkClient.readData(PATH));
+
+        // ---------------- 删除节点 -----------
+        zkClient.deleteRecursive(PATH);
+
+        // ---------------- 判断节点存在性 -----------
+        boolean exists = zkClient.exists(PATH);
+        System.out.println("节点：" + PATH + "仍然存在吗？" + exists);
+    }
 }
